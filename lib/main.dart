3e3ff1fa6403ab4c81d564065f833bbffdf04f3f
bryanRQ14 <37058101+bryanRQ14@users.noreply.github.com>
2018-05-04 17:32:04 -0500

    agregar

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async{
+  List _data= await getJson();
+  print(_data[0]['title']);
+
+  for(int i=0;i<_data.length;i++){
+    print(_data[i]['title']);
+  }
+  runApp(
+    new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("JSON Parse"),
+          centerTitle: true,
+          backgroundColor: Colors.orangeAccent,
+        ),
+        body: new Center(
+          child: new ListView.builder(
+              itemCount: _data.length,
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (BuildContext content, int position){
+                final index = position ~/2;
+                if(position.isOdd) return new Divider();
+                return  new ListTile(
+                  title: new Text("${_data[index]['title']}",
+                  style: new TextStyle(fontSize: 14.9),),
+                  subtitle: new Text("${_data[index]['body']}",
+                  style: new TextStyle(
+                    color: Colors.grey,
+                    fontSize: 13.4,
+                    fontStyle: FontStyle.italic
+                  ),
+                  ),
+                  leading: new CircleAvatar(
+                    backgroundColor: Colors.green,
+                    child: new Text("${_data[index]['body'][0].toString().toUpperCase()}",
+                      style: new TextStyle(
+                        fontSize: 13.4,
+                        color: Colors.orangeAccent
+                      ),
+
+                    ),
+                  ),
+                  onTap: (){_showOnTapMessage(content,"${_data[index]['title']}");},
+                );
+              },
+          )
+        ),
+      ),
+    )
+  );
+}
+
+void _showOnTapMessage(BuildContext context, String message){
+  var alert = new AlertDialog(
+    title: new Text('App'),
+    content: new Text(message),
+    actions: <Widget>[
+      new FlatButton(
+          onPressed: (){Navigator.pop(context);},
+          child: new Text('OK'))
+    ],
+  );
+  showDialog(context: context, child: alert);
+}
+
+
+
+
+Future<List> getJson() async{
+  String apiUrl="https://jsonplaceholder.typicode.com/posts";
+  http.Response response=await http.get(apiUrl);
+  return JSON.decode(response.body);
+}
